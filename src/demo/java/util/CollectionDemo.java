124a893a3dd52282d41ba836ad1dbd9d29dc1e36
micstart <hankers_wang@qq.com>
2022-11-25 16:32:02 +0800

    CollectionDemo.compare支持泛型

--- src/demo/java/util/CollectionDemo.java
+++ src/demo/java/util/CollectionDemo.java
@@ -6,7 +6,7 @@ public class CollectionDemo {
 
     public static void main(String[] args) {
         /* 固定长度列表 */
-        // List<Integer> oldList = Arrays.asList(1, 2, 3, 4, 5, 6);
+//        List<Integer> oldList = Arrays.asList(1, 2, 3, 4, 5, 6);
         /* 可变列表1 */
         List<Integer> oldList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
         oldList.add(null);
@@ -16,9 +16,9 @@ public class CollectionDemo {
         Collections.addAll(newList, 4, 5, 6, 7, 8, 9);
         newList.add(null);
 
-        Collection[] result = compare(oldList, newList, true);
+        List<Collection<Integer>> result = compare(oldList, newList, true);
         System.out.println("oldList=" + oldList + ", newList=" + newList);
-        System.out.println("result=" + Arrays.toString(result));
+        System.out.println("result=" + result);
     }
 
     /**
@@ -27,20 +27,22 @@ public class CollectionDemo {
      * @param oldCollection 旧数据集
      * @param newCollection 新数据集
      * @param needSame      是否需要相同部分
-     * @return
+     * @return result[0]新增值，result[1]删除值，result[2]相同部分
      */
-    public static Collection[] compare(final Collection oldCollection, final Collection newCollection, boolean needSame) {
-        Collection onlyInOld = new ArrayList(oldCollection);
-        Collection onlyInNew = new ArrayList(newCollection);
+    public static <T> List<Collection<T>> compare(final Collection<T> oldCollection, final Collection<T> newCollection, boolean needSame) {
+        List<Collection<T>> result = new ArrayList<>();
+        Collection<T> onlyInOld = new ArrayList<>(oldCollection);
+        Collection<T> onlyInNew = new ArrayList<>(newCollection);
         onlyInOld.removeAll(newCollection);
         onlyInNew.removeAll(oldCollection);
-        if (!needSame) {
-            return new Collection[]{onlyInNew, onlyInOld};
+        result.add(onlyInNew);
+        result.add(onlyInOld);
+        if (needSame) {
+            Collection<T> sameCollection = new ArrayList<>(oldCollection);
+            sameCollection.retainAll(newCollection);
+            result.add(sameCollection);
         }
-
-        Collection sameCollection = new ArrayList(oldCollection);
-        sameCollection.retainAll(newCollection);
-        return new Collection[]{onlyInNew, onlyInOld, sameCollection};
+        return result;
     }
 
 }
